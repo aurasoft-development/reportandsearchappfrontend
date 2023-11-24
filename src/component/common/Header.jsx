3fe656a9a94591da19defef1972646a3e6faabdc
Vikas <vikas.aurasoft@gmail.com>
2023-11-24 20:39:09 +0530

    responsive

--- src/component/common/Header.jsx
+++ src/component/common/Header.jsx
@@ -5,10 +5,14 @@ import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import { Link } from "react-router-dom";
 import QueryStatsIcon from '@mui/icons-material/QueryStats';
-import { Tab, Tabs } from '@mui/material';
+import { Tab, Tabs, Typography, useMediaQuery, useTheme } from '@mui/material';
 import '../../assets/css/Header.css'
+import DrawerCo from './DrawerCo';
 
 const Header = () => {
+    const theme = useTheme()
+    const isMatch = useMediaQuery(theme.breakpoints.down('md'))
+
     return (
         <>
             <div className='hedaerParent' >
@@ -23,14 +27,25 @@ const Header = () => {
                             >
                                 <QueryStatsIcon style={{ fontSize: '1em', color: 'white' }} />
                             </IconButton>
-                            <Tab label="Search And Report Engine" className='headerText' />
-                            <Box sx={{ flexGrow: 1 }} />
-                            <Tabs textColor='inherit' indicatorColor='secondary' value={false} >
-                                <Tab label="Home" component={Link} to="/" className='headerText' style={{ fontSize: '1em', color: 'white' }}/>
-                                <Tab label="Categories" component={Link} to="/" className='headerText' />
-                                <Tab label="Register" component={Link} to="/" className='headerText' />
-                                <Tab label="Help" component={Link} to="/" className='headerText' />
-                            </Tabs>
+                            {
+                                isMatch ? (
+                                    <>
+                                  <Tab label="Search And Report Engine" className='headerText' />
+                                    <DrawerCo />
+                                    </>
+                                ) :
+                                    <>
+                                        <Tabs textColor='inherit' indicatorColor='secondary' value={false} >
+                                            <Tab label="Home" component={Link} to="/" className='headerText' />
+                                            <Tab label="Categories" component={Link} to="/" className='headerText' />
+                                            <Tab label="Register" component={Link} to="/" className='headerText' />
+                                            <Tab label="Help" component={Link} to="/" className='headerText' />
+                                        </Tabs>
+                                        <Box sx={{ flexGrow: 1 }} />
+
+                                        <Tab label="Search And Report Engine" className='headerText' />
+                                    </>
+                            }
                         </Toolbar>
                     </AppBar>
                 </Box>
--- src/component/home/Tabs.jsx
+++ src/component/home/Tabs.jsx
@@ -136,20 +136,20 @@ export default function BasicTabs() {
                                 </Tabs>
                             </Box>
                             <div className='tab_div_main'>
-                                <div className='report-details'>
+                                <div className='report-details mainParentSeachBox'>
                                     <span><CategoryModel data={1} /></span>
                                     <div className="search-input">
                                         <TextField
                                             type="search"
                                             autoComplete="off"
-                                            style={{ width: '250px', color: 'white !important' }}
                                             id="search"
                                             placeholder="Enter UID"
+                                            className='SearchTextfield'
                                             color="success"
                                             label="Select category and search"
                                             onChange={(e) => setSearhData(e.target.value)}
                                         />
-                                        <Button variant="contained" color="success" className='mx-2' onClick={() => handleSearch(searchData)}>Search</Button>
+                                        <Button variant="contained" color="success" className='searchButton mx-2' onClick={() => handleSearch(searchData)}>Search</Button>
                                     </div>
                                 </div>
                                 <div className="user-details">
@@ -201,19 +201,19 @@ export default function BasicTabs() {
                                 </Tabs>
                             </Box>
                             <div className='tab_div_main'>
-                                <div className='report-details'>
+                                <div className='report-details mainParentSeachBox'>
                                     <span><CategoryModel data={2} /></span>
                                     <div className="search-input">
                                         <TextField
                                             type="search"
                                             autoComplete="off"
-                                            style={{ width: '250px' }}
+                                            className='SearchTextfield'
                                             id="search"
                                             placeholder="Enter UID"
                                             label="Select category and search"
                                             onChange={(e) => setSearhData2(e.target.value)}
                                         />
-                                        <Button variant="contained" color="success" className='mx-2' onClick={() => handleSearch2(searchData2)}>Search</Button>
+                                        <Button variant="contained" color="success" className='searchButton mx-2' onClick={() => handleSearch2(searchData2)}>Search</Button>
                                     </div>
                                 </div>
                                 <div className="user-details">
@@ -264,19 +264,19 @@ export default function BasicTabs() {
                                 </Tabs>
                             </Box>
                             <div className='tab_div_main'>
-                                <div className='report-details'>
+                                <div className='report-details mainParentSeachBox'>
                                     <span><CategoryModel data={3} /></span>
                                     <div className="search-input">
                                         <TextField
                                             type="search"
                                             autoComplete="off"
-                                            style={{ width: '250px' }}
                                             id="search"
                                             placeholder="Enter UID"
+                                            className='SearchTextfield'
                                             label="Select category and search"
                                             onChange={(e) => setSearhDat3(e.target.value)}
                                         />
-                                        <Button variant="contained" color="success" className='mx-2' onClick={() => handleSearch3(searchData3)}>Search</Button>
+                                        <Button variant="contained" color="success" className='searchButton mx-2' onClick={() => handleSearch3(searchData3)}>Search</Button>
                                     </div>
                                 </div>
                                 <div className="user-details">
@@ -327,19 +327,19 @@ export default function BasicTabs() {
                                 </Tabs>
                             </Box>
                             <div className='tab_div_main'>
-                                <div className='report-details'>
+                                <div className='report-details mainParentSeachBox'>
                                     <span><CategoryModel data={4} /></span>
                                     <div className="search-input">
                                         <TextField
                                             type="search"
                                             autoComplete="off"
-                                            style={{ width: '250px' }}
                                             id="search"
                                             placeholder="Enter UID"
+                                            className='SearchTextfield'
                                             label="Select category and search"
                                             onChange={(e) => setSearhData4(e.target.value)}
                                         />
-                                        <Button variant="contained" color="success" className='mx-2' onClick={() => handleSearch4(searchData4)}>Search</Button>
+                                        <Button variant="contained" color="success" className='searchButton mx-2' onClick={() => handleSearch4(searchData4)}>Search</Button>
                                     </div>
                                 </div>
                                 <div className="user-details">
