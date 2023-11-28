664a659c4d8494fb94e342071e4881a40981f5b8
deepesh kumar <deepesh.aurasoft@gmail.com>
2023-11-28 11:37:44 +0530

    add comment  model,view_details,home,app,firebase,main

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,6 +1,8 @@
+// Import Firebase modules for initialization and authentication
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
+// Firebase configuration containing API keys and project details
 const firebaseConfig = {
   apiKey: "AIzaSyAfivjqBE2vhu7_jKUNHtpZjh6jEzevUIE",
   authDomain: "search-and-find-application.firebaseapp.com",
@@ -11,7 +13,11 @@ const firebaseConfig = {
   measurementId: "G-EHP2N4P6BQ"
 };
 
-// Initialize Firebase
+// Initialize Firebase app with the provided configuration
 const app = initializeApp(firebaseConfig);
+
+// Get the authentication instance from the initialized Firebase app
 export const auth = getAuth(app);
+
+// Export the initialized Firebase app
 export default app;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,24 @@
+// Import necessary React modules and dependencies
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client'
+
+// Import the main App component
 import App from './App.jsx'
+
+// Import the FindProvider context component
 import FindProvider from './context/FindContext.jsx'
 
+// Use ReactDOM.createRoot to render the App component within the specified root element
 ReactDOM.createRoot(document.getElementById('root')).render(
+
+   // Wrap the entire application in the FindProvider context for state management
   <FindProvider>
+
+    {/* Use BrowserRouter for client-side routing */}
     <BrowserRouter>
+
+    {/* Render the main App component */}
       <App />
     </BrowserRouter>
   </FindProvider>,
