a1a2f9bab0c11360f0171c795ad65d5031fe2e41
deepesh kumar <deepesh.aurasoft@gmail.com>
2023-12-12 12:36:54 +0530

    remove captch and firebase version used 9.23.0 applay lodader

--- src/component/home/OtpVarifacation.jsx
+++ src/component/home/OtpVarifacation.jsx
@@ -6,96 +6,157 @@ import 'react-phone-number-input/style.css'
 import { Alert, Button } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 import { FindState } from '../../context/FindContext'
+import { toast } from 'react-toastify'
+import { auth } from "../../firebase";
+import { RecaptchaVerifier, signInWithPhoneNumber } from "firebase/auth";
+import CircularProgress from "@mui/material/CircularProgress";
 
 // Functional component for OTP verification
 const OtpVarifacation = () => {
 
-    // State variables to manage phone number, OTP input, flags, error messages, and confirmation object
-    const [number, setNumber] = useState()
-    const [otp, setOtp] = useState("")
-    const [flag, setFlag] = useState(false);
-    const [error, setError] = useState("");
-    const [confirmObj, setConfirmObj] = useState("");
-    const [data, setData] = useState()
+  // State variables to manage phone number, OTP input, flags, error messages, and confirmation object
+  const [number, setNumber] = useState()
+  const [otp, setOtp] = useState("")
+  const [flag, setFlag] = useState(false);
+  const [error, setError] = useState("");
+  const [confirmObj, setConfirmObj] = useState("");
+  const [data, setData] = useState()
+  const [loading, setLoading] = useState(false)
 
-    // Accessing state and functions from context and navigation hook
-    const { setUpRecaptcha, step, setStep } = FindState();
-    const navigate = useNavigate()
+  // Accessing state and functions from context and navigation hook
+  const { setUpRecaptcha, step, setStep } = FindState();
+  const navigate = useNavigate()
 
-    // useEffect to retrieve category1 data from local storage on component mount
-    useEffect(() => {
-        const Info = JSON.parse(localStorage.getItem('category1'))
-        setData(Info)
-    }, [])
+  // useEffect to retrieve category1 data from local storage on component mount
+  useEffect(() => {
+    const Info = JSON.parse(localStorage.getItem('category1'))
+    setData(Info)
+  }, [])
 
-    // Function to request OTP with the provided phone number
-    const getOtp = async (number) => {
-        setError("");
-        if (number === "" || number === undefined)
-            return setError("Please enter a valid phone number.")
-        try {
-            const response = await setUpRecaptcha(number);
-            setConfirmObj(response)
-            setFlag(true)
+  // // Function to request OTP with the provided phone number
+  // const getOtp = async (number) => {
+  //     setError("");
+  //     if (number === "" || number === undefined)
+  //         return setError("Please enter a valid phone number.")
+  //     try {
+  //         const response = await setUpRecaptcha(number);
+  //         setConfirmObj(response)
+  //         setFlag(true)
 
-        } catch (err) {
-            setError(err.message)
-        }
+  //     } catch (err) {
+  //         setError(err.message)
+  //     }
+  // }
+
+  // // Function to verify the entered OTP
+  // const verifyOtp = async () => {
+  //     if (otp === "" || otp === null) return;
+  //     try {
+  //         setError("")
+  //         await confirmObj.confirm(otp)
+  //         setStep(step + 1)
+  //     } catch (err) {
+  //         setError(err.message)
+  //     }
+  // }
+
+  function onCaptchVerify() {
+    if (!window.recaptchaVerifier) {
+      window.recaptchaVerifier = new RecaptchaVerifier(
+        "recaptcha-container",
+        {
+          size: "invisible",
+          callback: (response) => {
+            onSignup();
+          },
+          "expired-callback": () => { },
+        },
+        auth
+      );
     }
+  }
 
-    // Function to verify the entered OTP
-    const verifyOtp = async () => {
-        if (otp === "" || otp === null) return;
-        try {
-            setError("")
-            await confirmObj.confirm(otp)
-            setStep(step + 1)
-        } catch (err) {
-            setError(err.message)
-        }
-    }
+  function onSignup() {
+    setLoading(true)
+    onCaptchVerify();
 
-    // Render the OTP verification component
-    return (
-        // <div className='opt_container'>
-            <div className='otp_wrapper'>
-                {/* Display error message if present */}
-                {error && <Alert variant="danger">{error}</Alert>}
+    const appVerifier = window.recaptchaVerifier;
 
-                {/* Display section for sending OTP */}
-                <div style={{ display: !flag ? "block" : "none" }} >
-                    <div className='send_otp'>
-                        <PhoneInput
-                            placeholder="Enter phone number"
-                            value={number}
-                            defaultCountry='IN'
-                            onChange={setNumber}
-                        />
-                        <div className='btn-div'>
-                            <Button variant="contained" className='mainButton' onClick={() => getOtp(number)}>Send Otp</Button>
-                        </div>
+    // const formatPh = "+" + ph;
 
-                        {/* Container for reCAPTCHA */}
-                        <div id='recaptcha-container'></div>
-                    </div>
-                </div>
+    signInWithPhoneNumber(auth, number, appVerifier)
+      .then((confirmationResult) => {
+        window.confirmationResult = confirmationResult;
+        setLoading(false)
+        toast.success("OTP sended successfully!");
+        setFlag(true)
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false)
+      });
+  }
 
-                {/* Display section for verifying OTP */}
-                <div className='verify_otp'
-                    style={{ display: flag ? "block" : "none" }}
-                >
-                    <div className='otp-btn'>
-                        <input type='text'
-                            placeholder="Enter otp"
-                            onChange={(e) => setOtp(e.target.value)}
-                        />
-                        <div className='verify-otp-btn'> <Button className='mainButton' variant="contained" onClick={() => verifyOtp()}>verify otp</Button></div>
-                    </div>
-                </div>
-            </div>
-        // </div>
+  function onOTPVerify() {
+    setLoading(true)
+    window.confirmationResult
+      .confirm(otp)
+      .then(async (res) => {
+        setLoading(false)
+        setStep(step + 1)
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false)
+      });
+  }
 
-    )
+
+  // Render the OTP verification component
+  return (
+    // <div className='opt_container'>
+    <div className='otp_wrapper'>
+      {/* Display error message if present */}
+      {error && <Alert variant="danger">{error}</Alert>}
+
+      {/* Display section for sending OTP */}
+      <div style={{ display: !flag ? "block" : "none" }} >
+        <div className='send_otp'>
+          <PhoneInput
+            placeholder="Enter phone number"
+            value={number}
+            defaultCountry='IN'
+            onChange={setNumber}
+          />
+          <div className='btn-div'>
+            {loading == true ? <CircularProgress /> : ""}
+            <Button variant="contained" className='mainButton' onClick={() => onSignup()}>Send Otp</Button>
+          </div>
+
+          {/* Container for reCAPTCHA */}
+          <div id='recaptcha-container'></div>
+        </div>
+      </div>
+
+      {/* Display section for verifying OTP */}
+      <div className='verify_otp'
+        style={{ display: flag ? "block" : "none" }}
+      >
+        <div className='otp-btn'>
+          <input type='text'
+            placeholder="Enter otp"
+            onChange={(e) => setOtp(e.target.value)}
+          />
+          <div className='verify-otp-btn'>
+            {loading == true ? <CircularProgress /> : ""}
+            <Button className='mainButton' variant="contained" onClick={() => onOTPVerify()}>verify otp</Button>
+          </div>
+        </div>
+      </div>
+    </div>
+    // </div>
+
+  )
 }
 
 // Exporting the OTP verification component
