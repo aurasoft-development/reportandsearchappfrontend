68d1e8f1dcd84e5ad4af942cb333dfc09b336d8f
Vikas <vikas.aurasoft@gmail.com>
2023-12-13 12:18:41 +0530

    issue fixed

--- src/component/home/Tabs.jsx
+++ src/component/home/Tabs.jsx
@@ -242,7 +242,6 @@ export default function BasicTabs() {
 
                                             <div>
 
-
                                             </div>
                                         </div>
                                     </div>
